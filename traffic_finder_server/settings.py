70e3dae185c0736c62c536e478e4dd072e87eec9
Kavan Liang <kavan.liang@mail.utoronto.ca>
2020-10-25 14:54:47 -0400

    Change to postgresql.

--- traffic_finder_server/settings.py
+++ traffic_finder_server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,6 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
+# TODO: Regenerate and hide secret
 SECRET_KEY = '@%sd)$e!i@@mwjrrbce^upr&v+7$nzhs$%omd2*q8$t16lqz-0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -73,13 +76,31 @@ WSGI_APPLICATION = 'traffic_finder_server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
+if 'RDS_HOSTNAME' in os.environ:
+    #Defined on Elastic Beanstalk EC2 Instances
+    DATABASES = {
+        #USE RDS; Should be read only.
+        'default': {
+            'ENGINE': 'django.db.backends.',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT']
+        }
+    }
+else: 
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #DEFAULT TO LOCAL POSTGRES
+        #SETUP YOUR OWN LOCAL DB
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'trafficfinderroutes',
+        'TEST': {
+            'Name': 'trafficfinderroutestest'
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
