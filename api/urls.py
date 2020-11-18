bb0b9af40dc3b2d38c7522ce1e5b96f418ce477a
Sajjad Mirza <sajjad.mirza@mail.utoronto.ca>
2020-11-17 23:13:33 -0500

    removed hardcoded index

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', hdc_index),
     path('getRoute', get_route),
     path('insertNode', insert_node),
     path('modifyNode', modify_node),
