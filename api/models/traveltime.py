93663bcced069b9951d524ad9d7df9219c7b835b
Kavan Liang <kavan.liang@mail.utoronto.ca>
2020-11-18 23:03:08 -0500

    Revert "Revert "Merge pull request #4 from CSC301-TTS-Project/aggregations""
    This reverts commit c8551b53

--- api/models/traveltime.py
+++ api/models/traveltime.py
@@ -1,6 +1,6 @@
 from django.db import models
 from datetime import datetime
-from .link import Link
+from django.db import connection
 
 
 class TravelTime(models.Model):
@@ -34,7 +34,6 @@ class TravelTime(models.Model):
     @staticmethod
     def get_data_for_route(link_dirs, date_range, days_of_week, hour_range):
         """
-        #TODO: min/max speed, percentile calculations, street names
 
         Return hourly aggregated data for the passed links.
 
@@ -42,18 +41,47 @@ class TravelTime(models.Model):
         @param date_range: String tuple corresponding to the date range e.g. ("2018-09-01", "2018-09-02")
         @param days_of_week: Tuple representing days of week to include in aggregation, e.g. [0, 1, 2] corresponds to sunday, monday, tuesday
         @param hour_range: An hourly range, e.g. [7, 13] for 7AM to 1PM
+
+        @precondition: link_dirs has at least one link.
         """
 
         start_time = datetime.strptime(f'{date_range[0]} {hour_range[0]}:00', '%Y-%m-%d %H:%M').replace(tzinfo=None)
         end_time = datetime.strptime(f'{date_range[1]} {hour_range[1]}:00', '%Y-%m-%d %H:%M').replace(tzinfo=None)
 
-        link_hourly = TravelTime.objects \
+        hourly = TravelTime.objects \
             .filter(link_dir__in=link_dirs) \
             .filter(tx__range=[start_time, end_time]) \
             .filter(tx__hour__range=hour_range) \
             .filter(tx__iso_week_day__in=days_of_week) \
             .extra({"hour": "date_trunc('hour', tx)::time"}) \
-            .values('link_dir', 'hour', 'length').annotate(hourly_mean_tt=models.Avg('mean'),
-                                                           link_obs=models.Count(1))
+            .values('hour') \
+            .annotate(link_obs=models.Count(1))
 
-        return link_hourly
+        # For whatever reason, the values length values in our DB aren't correct. Recalculate them and related values.
+        with connection.cursor() as cursor:
+            qs = ','.join('%s' for _ in range(len(link_dirs)))
+            cursor.execute(
+                f"SELECT SUM(length) "
+                f"FROM "
+                f"(SELECT DISTINCT links.link_dir, ST_Length(ST_Transform(links.wkb_geometry, 2952)) "
+                f"as length FROM links WHERE links.link_dir in ({qs})) as lt",
+                link_dirs)
+            total_length = cursor.fetchone()[0]
+            hourly = hourly.annotate(total_length=models.Value(total_length, models.FloatField())) \
+                .annotate(mean_speed=models.Avg('mean')) \
+                .annotate(std_dev_speed=models.StdDev('mean')) \
+                .annotate(mean_tt=((total_length / 1000) / models.Avg('mean')) * 3600) \
+                .annotate(std_dev_tt=((total_length / 1000) / models.StdDev('mean')) * 3600) \
+                .annotate(pct_85_speed=models.Aggregate(models.F("mean"),
+                                                        function="percentile_cont",
+                                                        template="%(function)s(0.85) WITHIN GROUP (ORDER BY %(expressions)s)")) \
+                .annotate(pct_95_speed=models.Aggregate(models.F("mean"),
+                                                        function="percentile_cont",
+                                                        template="%(function)s(0.95) WITHIN GROUP (ORDER BY %(expressions)s)")) \
+                .annotate(min_speed=models.Min('mean')) \
+                .annotate(max_speed=models.Max('mean'))
+            hourly = hourly.annotate(
+                full_link_obs=models.Value(((int((end_time - start_time).seconds) // 60) / 5) * len(link_dirs),
+                                           models.IntegerField()))
+
+        return hourly
