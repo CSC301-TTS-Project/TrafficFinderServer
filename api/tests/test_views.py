cd56c14d4d9bfd2b3f80893b0b83851467d1fc8a
Sarthak Saxena <sarthak.saxena@mail.utoronto.ca>
2020-12-11 02:20:01 -0500

    added authentication headers for all the tests

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -108,7 +108,7 @@ class ViewTest(TestCase):
         }), content_type="application/json", HTTP_AUTHORIZATION=token)
         self.assertEqual(response_2.status_code, 200)
 
-        response_3 = client.delete('/api/deleteNode', json.dumps({
+        response_3 = client.post('/api/deleteNode', json.dumps({
             'route': 0,
             'index': 1
         }), content_type="application/json", HTTP_AUTHORIZATION=token)
@@ -178,60 +178,68 @@ class ViewTest(TestCase):
         assert response.content.startswith(
             b'route_num,num_days,link_obs,min_speed,mean_speed,max_speed,pct_50_speed,pct_85_speed,pct_95_speed,std_dev_speed,min_tt,mean_tt,max_tt,std_dev_tt,total_length\n')
 
-    # def test_get_api_keys(self):
-    #     client = Client()
+    def test_get_api_keys(self):
+        client = Client()
+        response = client.post('/api/login_user',
+                               json.dumps({'username': "TestUser",
+                                           'password': "qwerty"}),
+                               content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        token = "Token " + json.loads(response.content)["token"]
+        response = client.post('/api/getKeys', HTTP_AUTHORIZATION=token)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(json.loads(response.content))
 
-    #     response = client.post('/api/getKeys')
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(json.loads(response.content), {
-    #         'HERE_PUBLIC_KEY': settings.HERE_PUBLIC_KEY,
-    #         'MAPBOX_PUBLIC_KEY': settings.MAPBOX_PUBLIC_KEY
-    #     })
+    def test_modify_route(self):
+        client = Client()
+        response = client.post('/api/login_user',
+                               json.dumps({'username': "TestUser",
+                                           'password': "qwerty"}),
+                               content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        token = "Token " + json.loads(response.content)["token"]
+        response_1 = client.post('/api/insertNode', json.dumps({
+            'route': 0,
+            'lat': 43.75079,
+            'lng': -79.63473,
+            'index': 0
+        }), content_type="application/json", HTTP_AUTHORIZATION=token)
+        self.assertEqual(response_1.status_code, 200)
 
-    # def test_modify_route(self):
-    #     client = Client()
-    #     response_1 = client.post('/api/insertNode', json.dumps({
-    #         'route': 0,
-    #         'lat': 43.75079,
-    #         'lng': -79.63473,
-    #         'index': 0
-    #     }), content_type="application/json")
-    #     self.assertEqual(response_1.status_code, 200)
+        response_2 = client.post('/api/insertNode', json.dumps({
+            'route': 0,
+            'lat': 43.749309,
+            'lng': -79.635237,
+            'index': 1
+        }), content_type="application/json", HTTP_AUTHORIZATION=token)
+        self.assertEqual(response_2.status_code, 200)
 
-    #     response_2 = client.post('/api/insertNode', json.dumps({
-    #         'route': 0,
-    #         'lat': 43.749309,
-    #         'lng': -79.635237,
-    #         'index': 1
-    #     }), content_type="application/json")
-    #     self.assertEqual(response_2.status_code, 200)
+        response_3 = client.post('/api/insertNode', json.dumps({
+            'route': 0,
+            'lat': 43.747832,
+            'lng': -79.632544,
+            'index': 2
+        }), content_type="application/json", HTTP_AUTHORIZATION=token)
+        self.assertEqual(response_3.status_code, 200)
 
-    #     response_3 = client.post('/api/insertNode', json.dumps({
-    #         'route': 0,
-    #         'lat': 43.747832,
-    #         'lng': -79.632544,
-    #         'index': 2
-    #     }), content_type="application/json")
-    #     self.assertEqual(response_3.status_code, 200)
+        response_4 = client.post('/api/insertNode', json.dumps({
+            'route': 0,
+            'lat': 43.747521,
+            'lng': -79.633854,
+            'index': 3
+        }), content_type="application/json", HTTP_AUTHORIZATION=token)
+        self.assertEqual(response_3.status_code, 200)
 
-    #     response_4 = client.post('/api/insertNode', json.dumps({
-    #         'route': 0,
-    #         'lat': 43.747521,
-    #         'lng': -79.633854,
-    #         'index': 3
-    #     }), content_type="application/json")
-    #     self.assertEqual(response_3.status_code, 200)
+        response_modify = client.post('/api/modifyNode', json.dumps({
+            'route': 0,
+            'index': 3,
+            'lat': 43.747965,
+            'lng': -79.631314,
+        }), content_type="application/json", HTTP_AUTHORIZATION=token)
+        self.assertEqual(response_modify.status_code, 200)
 
-    #     response_modify = client.patch('/api/modifyNode', json.dumps({
-    #         'route': 0,
-    #         'index': 3,
-    #         'lat': 43.747965,
-    #         'lng': -79.631314,
-    #     }), content_type="application/json")
-    #     self.assertEqual(response_modify.status_code, 200)
-
-    #     assert json.loads(response_modify.content) == {
-    #         "new_node": {"id": 1182083962, "lat": 43.74801, "lng": -79.63134},
-    #         "segment_updates": {"3": {"start_node": {"id": 30326160, "lat": 43.74774, "lng": -79.63243},
-    #                                   "end_node": {"id": 1182083962, "lat": 43.74801, "lng": -79.63134},
-    #                                   "coordinates": [[-79.63243, 43.74774], [-79.63183, 43.74789], [-79.63147, 43.74798], [-79.63134, 43.74801]]}}}
+        assert json.loads(response_modify.content) == {
+            "new_node": {"id": 1182083962, "lat": 43.74801, "lng": -79.63134},
+            "segment_updates": {"3": {"start_node": {"id": 30326160, "lat": 43.74774, "lng": -79.63243},
+                                      "end_node": {"id": 1182083962, "lat": 43.74801, "lng": -79.63134},
+                                      "coordinates": [[-79.63243, 43.74774], [-79.63183, 43.74789], [-79.63147, 43.74798], [-79.63134, 43.74801]]}}}
